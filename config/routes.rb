9df0471c23c026878c8c0b77794be79b499a9291
Damiano <damiano.giacomello@diginess.it>
2013-03-15 13:26:39 +0100

    fixed routing for error in promotions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
-Spree::Core::Engine.routes.prepend do
+Spree::Core::Engine.routes.append do
 
   namespace :admin do
 
     resources :relation_types
-    resources :products do
-      get :related, :on => :member
+    namespace :products do
+      get '/products/related', :to => "products#related"
       resources :relations
     end
 
